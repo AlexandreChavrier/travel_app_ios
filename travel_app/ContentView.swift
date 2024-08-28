c5c4af768503e09c1b4881a12852569f6f7dddb2
alexandre chavrier <alex69.chavrier@gmail.com>
2024-08-28 14:36:28 +0200

    Initial Commit

---
+++ travel_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  travel_app
+//
+//  Created by Alexandre Chavrier on 28/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ travel_app/travel_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  travel_appApp.swift
+//  travel_app
+//
+//  Created by Alexandre Chavrier on 28/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct travel_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
